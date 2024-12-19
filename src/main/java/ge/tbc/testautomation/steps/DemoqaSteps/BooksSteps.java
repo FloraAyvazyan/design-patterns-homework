df3a3080c4a7ebfe98beb0c2fd3452f1cd188dd2
Flora Ayvazyan <flora.ayvazyan@mail.ru>
2024-12-19 13:01:55 +0400

    finished allure homework

--- src/main/java/ge/tbc/testautomation/steps/DemoqaSteps/BooksSteps.java
+++ src/main/java/ge/tbc/testautomation/steps/DemoqaSteps/BooksSteps.java
@@ -4,17 +4,21 @@ import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
 import ge.tbc.testautomation.pages.DemoqaPages.BooksPage;
+import io.qameta.allure.Step;
 import org.testng.asserts.SoftAssert;
 import static com.codeborne.selenide.Condition.attribute;
 
 public class BooksSteps extends BooksPage {
     SoftAssert sfa = new SoftAssert();
 
+
+    @Step("Scroll to the books section on the page")
     public BooksSteps scrollToBooks() {
         bookPage.scrollTo();
         return this;
     }
 
+    @Step("Find all books by publisher: {publisher} and containing title: {title}")
     public BooksSteps findAllBooksWithPublisher(ElementsCollection elements, String publisher, String title) {
         elements =  allBooks.filterBy(Condition.innerText(publisher))
                 .filterBy(Condition.partialText(title));
@@ -22,6 +26,8 @@ public class BooksSteps extends BooksPage {
         return this;
 
     }
+
+    @Step("Find all books by publisher: {publisher} and title: {title} using 'findAll'")
     public BooksSteps findAllBooksWithPublisherWithFindAll(ElementsCollection elements, String publisher, String title){
          elements = booksBody
                 .findAll(".rt-tr-group")
@@ -32,6 +38,7 @@ public class BooksSteps extends BooksPage {
 
     }
 
+    @Step("Verify that all book images are not empty")
     public BooksPage imageAreNotEmpty(ElementsCollection elements) {
         for (SelenideElement book : withImg) {
             book.shouldHave(attribute("src"));
@@ -40,15 +47,19 @@ public class BooksSteps extends BooksPage {
 
     }
 
+    @Step("Perform a failed assertion to check the size of the book list")
     public BooksSteps failedAssertion(){
         sfa.assertEquals(allBooks.size(), 10);
         return this;
     }
 
+    @Step("Verify that the title of the first book is '{title}'")
     public BooksSteps firstBookTitle(String title){
         sfa.assertEquals(allBooks.get(0).getText(),  title);
         return this;
     }
+
+    @Step("Assert all collected assertions")
     public BooksSteps assertAll(){
         sfa.assertAll();
         return this;
--- src/main/java/ge/tbc/testautomation/steps/DemoqaSteps/TextBoxSteps.java
+++ src/main/java/ge/tbc/testautomation/steps/DemoqaSteps/TextBoxSteps.java
@@ -1,39 +1,49 @@
 package ge.tbc.testautomation.steps.DemoqaSteps;
 import ge.tbc.testautomation.pages.DemoqaPages.TextBoxPage;
+import io.qameta.allure.Step;
+
 import java.util.List;
 import static com.codeborne.selenide.CollectionCondition.exactTexts;
 
 public class TextBoxSteps extends TextBoxPage {
+    @Step("Scroll to the text box section")
     public TextBoxSteps textBoxScrollTo() {
         textBox.scrollTo();
         return this;
     }
 
+    @Step("Fill in the full name field with '{name}'")
     public TextBoxSteps fillFullName(String name) {
         fullName.setValue(name);
         return this;
     }
 
+    @Step("Fill in the email field with '{em}'")
     public TextBoxSteps fillEmail(String em) {
         email.setValue(em);
         return this;
     }
 
+    @Step("Fill in the current address field with '{address}'")
     public TextBoxSteps fullCurrentAddress(String address) {
         currentAddress.setValue(address);
         return this;
     }
 
+
+    @Step("Fill in the permanent address field with '{address}'")
     public TextBoxSteps fillPermanentAddress(String address) {
         permanentAddress.setValue(address);
         return this;
     }
 
+    @Step("Validate the form results: {result}")
     public TextBoxSteps validateRealResult(List<String> result) {
         realResult.shouldHave(exactTexts(result));
         return this;
     }
 
+    @Step("Scroll to the submit button and click it")
     public TextBoxSteps submitButtonScrollToAndClick() {
         submitButton.scrollTo().click();
         return this;
